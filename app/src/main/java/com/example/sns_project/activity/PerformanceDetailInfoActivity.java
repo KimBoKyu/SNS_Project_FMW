3856c529e77151f547a3013b030588353f14ace9
KimBoKyu <sa5469@naver.com>
2019-08-26 19:01:59 +0900

    oo

--- app/src/main/java/com/example/sns_project/activity/PerformanceDetailInfoActivity.java
+++ app/src/main/java/com/example/sns_project/activity/PerformanceDetailInfoActivity.java
@@ -1,6 +1,9 @@
 package com.example.sns_project.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -15,28 +18,39 @@ import com.google.android.gms.maps.model.MarkerOptions;
 ;
 
 public class PerformanceDetailInfoActivity extends FragmentActivity implements OnMapReadyCallback {
-
     private GoogleMap mMap;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_performance_detail);
-
+        findViewById(R.id.buttonBuyLink).setOnClickListener(onClickListener);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.buttonBuyLink:
+                    gotoLink();
+                    break;
+            }
+        }
+    };
+
+    public void gotoLink(){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ticketlink.co.kr/product/15363"));
+        startActivity(intent);
+    }
+
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
-
         LatLng SEOUL = new LatLng(37.5657855355, 126.967578144);
-
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
