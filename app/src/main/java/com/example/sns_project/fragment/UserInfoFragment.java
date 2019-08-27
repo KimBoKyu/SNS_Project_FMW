f828be9f35de27e1693f038f9571338899f86416
LEESUJIN <slrkajsep337@naver.com>
2019-08-27 14:03:16 +0900

    27Aug2019_sujin_incomplete_setOnItemClickListner

--- app/src/main/java/com/example/sns_project/fragment/UserInfoFragment.java
+++ app/src/main/java/com/example/sns_project/fragment/UserInfoFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -30,12 +31,19 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class UserInfoFragment extends Fragment {
+
+
+
+    //  *** 내가 쓴 게시물 알파벳순 정렬 -> 시간순 정렬로 수정 해야함 ***
+
 
 
 
@@ -104,6 +112,9 @@ public class UserInfoFragment extends Fragment {
             }
         });
 
+
+
+
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
         CollectionReference posts = FirebaseFirestore.getInstance().collection("posts");
         posts.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -134,13 +145,18 @@ public class UserInfoFragment extends Fragment {
 
 
 
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView parent, View v, int position, long id) {
-//
-//
-//            }
-//        }) ;
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+
+
+                //Toast.makeText(getContext(),,Toast.LENGTH_LONG).show();
+
+
+
+
+            }
+        }) ;
 
 
 
